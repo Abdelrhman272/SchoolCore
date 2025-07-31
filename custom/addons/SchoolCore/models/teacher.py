fb9e6b032a7dedf09672e2778e3ede7ffcf3d7cb
Abdelrhman Elsayed <info@nextgensystems.net>
2025-07-31 12:06:05 +0300

    feat: completed teacher module with user, subjects, classes, views and menu

--- custom/addons/SchoolCore/models/teacher.py
+++ custom/addons/SchoolCore/models/teacher.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class EduTeacher(models.Model):
     _name = 'edu.teacher'
@@ -6,8 +6,11 @@ class EduTeacher(models.Model):
     _inherit = ['mail.thread']
 
     name = fields.Char(required=True)
-    employee_ref = fields.Many2one('hr.employee')
-    subject_ids = fields.Many2many('edu.subject')
+    employee_ref = fields.Many2one('hr.employee', string="Employee")
+    user_id = fields.Many2one('res.users', string="User Account")
     phone = fields.Char()
     email = fields.Char()
     active = fields.Boolean(default=True)
+
+    subject_ids = fields.Many2many('edu.subject', string="Subjects")
+    class_ids = fields.Many2many('edu.class', string="Classes")
